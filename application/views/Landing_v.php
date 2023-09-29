e4d08ff243e7ef7c02a310a72ae79a1a2e1b0fa9
dev3606 <jd3606192@gmail.com>
2023-09-29 03:32:07 -0400

    landing page

---
+++ application/views/Landing_v.php
@@ -0,0 +1,297 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seamotech</title>
+    <link rel="shortcut icon" type="image/png" href="<?php echo base_url('assets/img/favicon.png')?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/landing.min.css')?>">
+</head>
+
+<body data-aos-easing="ease" data-aos-duration="400" data-aos-delay="0">
+    <div class="page-wrapper p-0 overflow-hidden">
+        <header class="header">
+            <nav class="navbar navbar-expand-lg py-0">
+                <div class="container">
+                    <a class="navbar-brand me-0 py-0" href="<?php echo base_url()?>">
+                        <img src="<?php echo base_url('assets/img/dark-logo.png')?>" alt="img-fluid"
+                            style="width:195px;padding-top:5px;">
+                    </a>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav align-items-center mb-2 mb-lg-0 ms-auto">
+                            <li class="nav-item ms-2">
+                                <a class="btn btn-primary fs-3 rounded btn-hover-shadow px-3 py-2"
+                                    href="<?php echo base_url('login')?>">Login</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <div class="body-wrapper overflow-hidden">
+            <section class="hero-section position-relative overflow-hidden mb-0 mb-lg-11">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-6">
+                            <div class="hero-content my-11 my-xl-0">
+                                <h6 class="d-flex align-items-center gap-2 fs-4 fw-semibold mb-3 aos-init aos-animate"
+                                    data-aos="fade-up" data-aos-delay="200" data-aos-duration="1000"><i
+                                        class="ti ti-rocket text-secondary fs-6"></i>Increase revenue, nurture customer relationships &
+analyse performance with our award-winning CRM platform</h6>
+                                <h1 class="fw-bolder mb-8 fs-13 aos-init aos-animate" data-aos="fade-up"
+                                    data-aos-delay="400" data-aos-duration="1000">Most powerful &amp; <span
+                                        class="text-primary"> All in One CRM</span>
+                                        Affordable</h1>
+                                <p class="fs-5 mb-5 text-dark fw-normal aos-init aos-animate" data-aos="fade-up"
+                                    data-aos-delay="600" data-aos-duration="1000">Make the most of Agile's modern features & integrations, easy implementation and great support at an affordable price.</p>
+                                <div class="d-sm-flex align-items-center gap-3 aos-init aos-animate" data-aos="fade-up"
+                                    data-aos-delay="800" data-aos-duration="1000">
+                                    <a class="btn btn-primary px-5 btn-hover-shadow d-block mb-3 mb-sm-0"
+                                        href="<?php echo base_url('login')?>">Login</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 d-none d-xl-block">
+                            <div class="hero-img-slide position-relative bg-light-primary p-4 rounded">
+                                <div class="d-flex flex-row">
+                                    <div class="">
+                                        <div class="banner-img-1 slideup">
+                                            <img src="<?php echo base_url('assets/img/bannerimg1.png')?>" alt=""
+                                                class="img-fluid">
+                                        </div>
+                                        <div class="banner-img-1 slideup">
+                                            <img src="<?php echo base_url('assets/img/bannerimg1.png')?>" alt=""
+                                                class="img-fluid">
+                                        </div>
+                                    </div>
+                                    <div class="">
+                                        <div class="banner-img-2 slideDown">
+                                            <img src="<?php echo base_url('assets/img/bannerimg2.png')?>" alt=""
+                                                class="img-fluid">
+                                        </div>
+                                        <div class="banner-img-2 slideDown">
+                                            <img src="<?php echo base_url('assets/img/bannerimg2.png')?>" alt=""
+                                                class="img-fluid">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="production pb-10" id="production-template">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8 aos-init" data-aos="fade-up" data-aos-delay="200" data-aos-duration="1000">
+                            <h2 class="text-center mb-0 fs-9 fw-bolder">Production Ready &amp; Developer Friendly
+                                Bootstrap Admin
+                                Template</h2>
+                        </div>
+                    </div>
+                    <div class="domo-contect position-relative">
+                        <div class="demos-view mt-11 pt-lg-8">
+                            <div class="row justify-content-center">
+                                <div class="col-sm-6 col-lg-4 mb-8">
+                                    <div
+                                        class="border d-block rounded-1 mb-2 position-relative lp-demos-box overflow-hidden">
+                                        <img src="<?php echo base_url('assets/img/demo.png')?>" alt=""
+                                            class="img-fluid">
+                                    </div>
+                                    <h6 class="mb-0 text-center fs-3">New</h6>
+                                </div>
+                                <div class="col-sm-6 col-lg-4 mb-8">
+                                    <div
+                                        class="border d-block rounded-1 mb-2 position-relative lp-demos-box overflow-hidden">
+                                        <img src="<?php echo base_url('assets/img/demo2.png')?>" alt=""
+                                            class="img-fluid">
+                                    </div>
+                                    <h6 class="mb-0 text-center fs-3">Not Interested</h6>
+                                </div>
+                                <div class="col-sm-6 col-lg-4 mb-8">
+                                    <div
+                                        class="border d-block rounded-1 mb-2 position-relative lp-demos-box overflow-hidden">
+                                        <img src="<?php echo base_url('assets/img/demo3.png')?>" alt=""
+                                            class="img-fluid">
+                                    </div>
+                                    <h6 class="mb-0 text-center fs-3">Follow up</h6>
+                                </div>
+                                <div class="col-sm-6 col-lg-4 mb-8">
+                                    <div
+                                        class="border d-block rounded-1 mb-2 position-relative lp-demos-box overflow-hidden">
+                                        <img src="<?php echo base_url('assets/img/demo4.png')?>" alt=""
+                                            class="img-fluid">
+                                    </div>
+                                    <h6 class="mb-0 text-center fs-3">FTD</h6>
+                                </div>
+                                <div class="col-sm-6 col-lg-4 mb-8">
+                                    <div
+                                        class="border d-block rounded-1 mb-2 position-relative lp-demos-box overflow-hidden">
+                                        <img src="<?php echo base_url('assets/img/demo5.png')?>" alt=""
+                                            class="img-fluid">
+                                    </div>
+                                    <h6 class="mb-0 text-center fs-3">Unqualified</h6>
+                                </div>
+                                <div class="col-sm-6 col-lg-4 mb-8">
+                                    <div
+                                        class="border d-block rounded-1 mb-2 position-relative lp-demos-box overflow-hidden">
+                                        <img src="<?php echo base_url('assets/img/demo6.png')?>" alt=""
+                                            class="img-fluid">
+                                    </div>
+                                    <h6 class="mb-0 text-center fs-3">Wrong number</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="bg-light">
+                <div class="container-fluid">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8 col-xxl-6">
+                            <h2 class="fs-9 text-center mb-5 fw-bolder">Increase speed of your development and launch
+                                quickly
+                                with Modernize</h2>
+                        </div>
+                    </div>
+                    <div class="sliding-wrapper position-relative overflow-hidden">
+                        <div class="slide-background d-flex flex-row w-100">
+                            <div class="slide">
+                                <img src="<?php echo base_url('assets/img/slider-group.png')?>" alt="slide" height="100%">
+                            </div>
+                            <div class="slide">
+                                <img src="<?php echo base_url('assets/img/slider-group.png')?>" alt="slide" height="100%">
+                            </div>
+                            <div class="slide">
+                                <img src="<?php echo base_url('assets/img/slider-group.png')?>" alt="slide" height="100%">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="features-section py-5">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-6">
+                            <div class="text-center aos-init" data-aos="fade-up" data-aos-delay="600"
+                                data-aos-duration="1000">
+                                <small class="text-primary fw-bold mb-2 d-block fs-3">ALMOST COVERED EVERYTHING</small>
+                                <h2 class="fs-9 text-center mb-4 mb-lg-9 fw-bolder">Other Amazing Features &amp;
+                                    Flexibility Provided</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6 col-md-4 col-lg-3 aos-init" data-aos="fade-up" data-aos-delay="800"
+                            data-aos-duration="1000">
+                            <div class="text-center mb-5">
+                                <i class="d-block ti ti-wand text-primary fs-10"></i>
+                                <h5 class="fs-5 fw-semibold mt-8">6 Theme Colors</h5>
+                                <p class="mb-0 text-dark">We have included 6 pre-defined Theme Colors with Elegant
+                                    Admin.</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-3 aos-init" data-aos="fade-up" data-aos-delay="800"
+                            data-aos-duration="1000">
+                            <div class="text-center mb-5">
+                                <i class="d-block ti ti-layout-sidebar text-primary fs-10"></i>
+                                <h5 class="fs-5 fw-semibold mt-8">Dark &amp; Light Sidebar</h5>
+                                <p class="mb-0 text-dark">Included Dark and Light Sidebar for getting desire look and
+                                    feel.</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-3 aos-init" data-aos="fade-up" data-aos-delay="800"
+                            data-aos-duration="1000">
+                            <div class="text-center mb-5">
+                                <i class="d-block ti ti-archive text-primary fs-10"></i>
+                                <h5 class="fs-5 fw-semibold mt-8">425+ Page Templates</h5>
+                                <p class="mb-0 text-dark">Yes, we have 5 demos &amp; 79+ Pages per demo to make it
+                                    easier.</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-3 aos-init" data-aos="fade-up" data-aos-delay="800"
+                            data-aos-duration="1000">
+                            <div class="text-center mb-5">
+                                <i class="d-block ti ti-adjustments text-primary fs-10"></i>
+                                <h5 class="fs-5 fw-semibold mt-8">150+ UI Components</h5>
+                                <p class="mb-0 text-dark">Almost 150+ UI Components being given with Modernize Admin
+                                    Pack.</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-3 aos-init" data-aos="fade-up" data-aos-delay="1000"
+                            data-aos-duration="1000">
+                            <div class="text-center mb-5">
+                                <i class="d-block ti ti-tag text-primary fs-10"></i>
+                                <h5 class="fs-5 fw-semibold mt-8">Bootstrap 5x</h5>
+                                <p class="mb-0 text-dark">Its been made with Bootstrap 5 and full responsive layout.</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-3 aos-init" data-aos="fade-up" data-aos-delay="1000"
+                            data-aos-duration="1000">
+                            <div class="text-center mb-5">
+                                <i class="d-block ti ti-diamond text-primary fs-10"></i>
+                                <h5 class="fs-5 fw-semibold mt-8">200+ Font Icons</h5>
+                                <p class="mb-0 text-dark">Lots of Icon Fonts are included here in the package of Elegant
+                                    Admin.</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-3 aos-init" data-aos="fade-up" data-aos-delay="1000"
+                            data-aos-duration="1000">
+                            <div class="text-center mb-5">
+                                <i class="d-block ti ti-device-desktop text-primary fs-10"></i>
+                                <h5 class="fs-5 fw-semibold mt-8">Fully Responsive</h5>
+                                <p class="mb-0 text-dark">All the layout of Modernize Admin is Fully Responsive and
+                                    widely tested.</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-3 aos-init" data-aos="fade-up" data-aos-delay="1000"
+                            data-aos-duration="1000">
+                            <div class="text-center mb-5">
+                                <i class="d-block ti ti-database text-primary fs-10"></i>
+                                <h5 class="fs-5 fw-semibold mt-8">SassBase CSS</h5>
+                                <p class="mb-0 text-dark">Our Css is written Sass Base to make your life easier.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="bg-primary pt-5 pt-xl-9 pb-8">
+                <div class="container">
+                    <div class="row justify-content-between">
+                        <div class="col-lg-7 col-xl-5 pt-lg-5 mb-5 mb-lg-0">
+                            <h2 class="fs-12 text-white text-center text-lg-start fw-bolder mb-8">Build your app with
+                                our highly
+                                customizable Bootstrap based Dashboard</h2>
+                            <div
+                                class="d-sm-flex align-items-center justify-content-center justify-content-lg-start gap-3">
+                                <a href="<?php echo base_url('login')?>"
+                                    class="btn bg-white text-primary fw-semibold d-block mb-3 mb-sm-0 btn-hover-shadow">Login</a>
+                            </div>
+                        </div>
+                        <div class="col-lg-5 col-xl-5">
+                            <div class="text-center text-lg-end">
+                                <img src="<?php echo base_url('assets/img/business-woman-checking-her-mail.png')?>"
+                                    alt="" class="img-fluid">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </div>
+        <footer class="footer-part pt-8 pb-5">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-4">
+                        <div class="text-center">
+                            <p class="mb-0 text-dark">All rights reserved by Seamotech. Designed &amp; Developed by Seamotech</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</body>
+
+</html>
