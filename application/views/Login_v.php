da77661ab9f39c194671db111c1b972574fac76d
dev3606 <jd3606192@gmail.com>
2023-11-27 02:55:14 -0500

    Test webhooks

--- application/views/Login_v.php
+++ application/views/Login_v.php
@@ -32,8 +32,8 @@
                                 </a>
                                 <div>
                                     <div class="mb-3">
-                                        <label for="username" class="form-label">Username</label>
-                                        <input type="text" class="form-control" id="username"
+                                        <label for="username" class="form-label">User name</label>
+                                        <input type="text" class="form-control" id="username" placeholder="User name"
                                             aria-describedby="emailHelp">
                                     </div>
                                     <div class="mb-4">
